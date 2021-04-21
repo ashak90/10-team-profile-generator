9e7cf72479619ab138b13dfde03d9ec16b050a66
Asha Kumar <ashakumar90@gmail.com>
2021-04-21 14:59:57 -0400

    added added more questions for specific roles

--- index.js
+++ index.js
@@ -1,57 +1,78 @@
 const inquirer = require(`inquirer`);
 const fs = require(`fs`);
-const Employee = require("./lib/Employee")
-const style = require("./assets/style")
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const style = require("./assets/style");
 
-function init(){
-    // inquirer
-    //     .prompt(questions)
-    //     .then((answers) => {
-    //         console.log("Your responses:", answers)
-    //     })
-    startHtml()
-    addMember();
+function init() {
+  // inquirer
+  //     .prompt(questions)
+  //     .then((answers) => {
+  //         console.log("Your responses:", answers)
+  //     })
+  startHtml();
+  addMember();
 }
 
-function addMember () {
-   return inquirer.prompt([
-    {
+function addMember() {
+  return inquirer
+    .prompt([
+      {
         type: "input",
         name: "name",
         message: "Enter the team member's name:",
-    },
-    {
+      },
+      {
         type: "list",
         name: "role",
-        message:"Select the team member's role:",
-        choices: [
-            "Intern",
-            "Employee",
-            "Engineer",
-            "Manager" 
-        ]
-    },
-    {
-        type:"input",
-        name:"id",
-        message:"Enter team member's id:",
-    },
-    {
-        type:"input",
-        name:"email",
-        message:"Enter team member's email address:",
-    },
-    // {
-    //     type:"input",
-    //     name:"id",
-    //     message:"Enter team member's email address:",
-    // },
-])
+        message: "Select the team member's role:",
+        choices: ["Intern", "Employee", "Engineer", "Manager"],
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter team member's id:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter team member's email address:",
+      },
+      // {
+      //     type:"input",
+      //     name:"id",
+      //     message:"Enter team member's email address:",
+      // },
+    ])
+    .then(function ({ name, role, id, email }) {
+        let roleSpecs = "";
+        if (role === "Engineer"){
+            roleSpecs = "Github username";
+        } else if (role === "Intern"){
+            roleSpecs = "school name"
+        } else {
+            roleSpecs = "office phone number"
+        }
+        inquirer.prompt([{
+            message: `Enter the team member's ${roleSpecs}: `,
+            name: "role Specs"
+        },
+        {
+            type:  "list",
+            name: "more members",
+            message: "Would you like to add more team member's",
+            choices: [
+                "Yes",
+                "No"
+            ],
+        }
+    ])
+    });
 }
 
 function startHtml() {
-    const creatHtml = 
-    `<!DOCTYPE html>
+  const creatHtml = `<!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="UTF-8">
@@ -67,13 +88,12 @@ function startHtml() {
         <div class="container">
             <div class="row">`;
 
-    fs.writeFile("./output/teamprofile.html", creatHtml, function(err){
-        if (err) {
-            console.log(err);
-        }
-    });
-    console.log ("start");
+  fs.writeFile("./output/teamprofile.html", creatHtml, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  console.log("start");
 }
-
 
 init();
